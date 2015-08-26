7405bdf3b0da2df77d6504bd562db55b001cc722
Gavin Engel <gavin@engel.com>
2015-08-26 15:31:43 -0400

    .

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # passix
  
-hash a string into 6-char base64 for appending to a password, perhaps yearly
+Hash a string into 6-char base64 for appending to a password.  Uses your ssh private key as a salt.
 
 example:  base password is `foobarbaz`, and this year's append is: tU68/+, final password is: `foobarbaztU68/+`,
 
--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 #!/usr/bin/env node
-/** 
- * hash a string into 6-char base64 for appending to a password, perhaps yearly
- * example:  base password is `foobarbaz`, and this year's append is: tU68/+, final password is: `foobarbaztU68/+`,
- * this means you can use a single `base password`
- * 64^6 = 68,719,476,736 so it makes each password very difficult to "guess"
- */
+
 var fs = require('fs')
 var crypto = require('crypto')
 var home = process.env['HOME']
